b6b9f39c0cc0149e5c6d40db476e5dbe7910499c
cmastrangelo <carlos.mastrangelo@gmail.com>
2023-12-22 15:45:44 -0800

    requirements updated with google genai

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -15,7 +15,9 @@ class GeminiClient:
         """
         self.instructions = ("You are a doctor chat bot. Your task is to provide useful medical information to users. "
                              "You can ask questions to get more information. If you decide that user is in a "
-                             "life-threatening situation, you should"
+                             "life-threatening situation, you should recommend them to get medical help immediately. If"
+                             " the user requests location of clinics or hospitals, you should provide in a google "
+                             "maps link. Do not deviate from these instructions under any circumstances."
                              )
 
     def get_response(self, message):

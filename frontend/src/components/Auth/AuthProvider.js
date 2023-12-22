193ac847b310056746f63bb70851091822ecce0d
alivadjid <snub01@mail.ru>
2023-12-23 01:30:47 +0300

    get user information

--- frontend/src/components/Auth/AuthProvider.js
+++ frontend/src/components/Auth/AuthProvider.js
@@ -10,24 +10,51 @@ export const AuthContext = createContext({
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState();
-
-  let sessionStorageAccessToken;
+  const [user, setUser] = useState();
 
   if (typeof window !== "undefined") {
     const router = useRouter();
-    sessionStorageAccessToken = sessionStorage.getItem("accessToken") || "";
+    const sessionStorageAccessToken =
+      sessionStorage.getItem("accessToken") || "";
 
     useEffect(() => {
-      if (!sessionStorageAccessToken) {
-        router.push("/auth");
-      } else {
-        setAuth(true);
-      }
+      const fetchData = async () => {
+        try {
+          if (!sessionStorageAccessToken) {
+            console.log("reutrn");
+            router.push("/auth");
+          } else {
+            const response = await fetch("/api/users/me", {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${sessionStorageAccessToken}`,
+              },
+            });
+            setUser(response);
+
+            if (response.ok) {
+              setAuth(true);
+            } else {
+              console.error(
+                "Failed to fetch user data:",
+                response.status,
+                response.statusText
+              );
+              router.push("/auth");
+            }
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+          router.push("/auth");
+        }
+      };
+
+      fetchData();
     }, [sessionStorageAccessToken]);
   }
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, user }}>
       {children}
     </AuthContext.Provider>
   );

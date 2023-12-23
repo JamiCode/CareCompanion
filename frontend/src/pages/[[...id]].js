5b1bd7bf50a40f23c3b638774d069a2c24030a78
alivadjid <snub01@mail.ru>
2023-12-23 08:34:20 +0300

    one page

--- frontend/src/pages/[[...id]].js
+++ frontend/src/pages/[[...id]].js
@@ -1,15 +1,17 @@
 "use client";
-
+import { useRouter } from "next/router";
 import { AuthProvider } from "@/components/Auth/AuthProvider";
 import MainLayout from "@/app/layout";
 import Chat from "@/components/Chat";
 
 const home = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  console.log("route", id?.[0]);
   return (
     <AuthProvider>
-      <MainLayout>
-        <Chat />
-      </MainLayout>
+      <MainLayout>{id?.[0] ? <Chat /> : ""}</MainLayout>
     </AuthProvider>
   );
 };

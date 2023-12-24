562ab4402cf3f24019765b9009a1a1d89d48255a
alivadjid <snub01@mail.ru>
2023-12-24 14:52:27 +0300

    remove image

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -143,11 +143,6 @@ const chart = () => {
                     </span>
                   </div>
                 </div>
-                <img
-                  src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
-                  alt="My profile"
-                  className="w-6 h-6 rounded-full order-1"
-                />
               </div>
             </div>
           );

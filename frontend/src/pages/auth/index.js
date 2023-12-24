7abe4f5649088e0ad391ed6b2c37859769c624f8
alivadjid <snub01@mail.ru>
2023-12-24 11:03:48 +0300

    picture and signup form

--- frontend/src/pages/auth/index.js
+++ frontend/src/pages/auth/index.js
@@ -1,9 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import "./index.css";
 import { useRouter } from "next/router";
 
 const auth = () => {
+  const [isSignup, setIsSignup] = useState(false);
   const router = useRouter();
 
   async function onSubmit(event) {
@@ -30,17 +32,21 @@ const auth = () => {
     }
   }
 
+  function signupClick() {
+    setIsSignup(!isSignup);
+  }
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto w-40"
+            src="./care-bot.png"
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            {isSignup ? "Sign up" : "Sign in to your account"}
           </h2>
         </div>
 
@@ -73,14 +79,14 @@ const auth = () => {
                 >
                   Password
                 </label>
-                <div className="text-sm">
+                {/* <div className="text-sm">
                   <a
                     href="#"
                     className="font-semibold text-indigo-600 hover:text-indigo-500"
                   >
                     Forgot password?
                   </a>
-                </div>
+                </div> */}
               </div>
               <div className="mt-2">
                 <input
@@ -99,20 +105,18 @@ const auth = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Submit
+              </button>
+
+              <button
+                onClick={signupClick}
+                type="button"
+                className="flex mt-2 w-full justify-center rounded-md bg-gray px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {isSignup ? "Sign in" : "Sign up"}
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
     </>

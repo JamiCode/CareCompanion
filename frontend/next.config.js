b494478035b07b4860042bb1e73533047c8999f7
alivadjid <snub01@mail.ru>
2023-12-25 03:30:26 +0300

    remove consoles

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    this.compiler = {
+      removeConsole: process.env.NODE_ENV === "production",
+    };
     const destinationUrl =
       "https://carecompanion-production-a0ae.up.railway.app/";
     console.log("destinationUrl", destinationUrl);
--- frontend/src/pages/auth/index.js
+++ frontend/src/pages/auth/index.js
@@ -7,13 +7,13 @@ import { useRouter } from "next/router";
 const auth = () => {
   const [isRegistration, setIsRegistration] = useState(false);
   const [isShowMessage, setIsShowMessage] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(''); 
+  const [errorMessage, setErrorMessage] = useState("");
 
   const router = useRouter();
 
   async function onSubmit(event) {
     event.preventDefault();
-    setErrorMessage('')
+    setErrorMessage("");
 
     const formData = new FormData(event.currentTarget);
 
@@ -25,13 +25,11 @@ const auth = () => {
         "Content-Type": "application/json",
       };
       let formDataJson = {};
-      console.log(formDataJson);
+
       formData.forEach((value, key) => {
         formDataJson[key] = value;
       });
 
-      console.log("formDataJson", formDataJson, JSON.stringify(formDataJson));
-      console.log(formData);
       const fetchObject = isRegistration
         ? {
             method: "POST",
@@ -57,7 +55,7 @@ const auth = () => {
       console.log("resp", response);
       if (response) {
         const data = await response.json();
-        if(!data.detail){
+        if (!data.detail) {
           if (data.access_token) {
             sessionStorage.setItem("accessToken", data.access_token);
             router.push("/");
@@ -65,9 +63,8 @@ const auth = () => {
             setIsShowMessage(true);
             setIsRegistration(false);
           }
-        }
-        else if (data.detail){
-          setErrorMessage(data.detail)
+        } else if (data.detail) {
+          setErrorMessage(data.detail);
         }
       }
     } catch (error) {
@@ -78,11 +75,9 @@ const auth = () => {
   function showRegistration() {
     setIsRegistration(!isRegistration);
   }
-  
 
   return (
     <div className="bg-white text-black min-h-screen flex flex-col justify-center py-12 px-6 lg:px-8">
-      
       <div className="sm:mx-auto sm:w-full sm:max-w-md text-center">
         <img
           src="./care-bot.png"
@@ -107,14 +102,17 @@ const auth = () => {
         ) : (
           ""
         )}
-         {errorMessage && (
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-              <p>{errorMessage}</p>
-            </div>
+        {errorMessage && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <p>{errorMessage}</p>
+          </div>
         )}
 
         <form className="space-y-6" onSubmit={onSubmit} method="POST">
-          {isRegistration ? <Registration/> : <Signin />}
+          {isRegistration ? <Registration /> : <Signin />}
         </form>
 
         <p className="mt-8 text-center text-sm text-gray-400">
@@ -132,9 +130,6 @@ const auth = () => {
 };
 
 const Registration = () => {
-  
-
- 
   return (
     <>
       {/* Email field */}

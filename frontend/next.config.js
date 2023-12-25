fdbc8c50c53c048717894ce1e19d238c773f0309
alivadjid <snub01@mail.ru>
2023-12-25 23:34:07 +0300

    prod url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
     this.compiler = {
       removeConsole: process.env.NODE_ENV === "production",
     };
+    console.log("h", process.env.API_URL);
+    const chech =
+      process.env.NODE_ENV === "development"
+        ? process.env.API_URL_DEV
+        : process.env.API_URL_PROD;
+
+    console.log("chech", chech);
     const destinationUrl =
       "https://carecompanion-production-a0ae.up.railway.app/";
     console.log("destinationUrl", destinationUrl);
